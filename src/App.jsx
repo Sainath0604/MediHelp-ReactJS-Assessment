666543a2c9baf387e3616206a1a40c3e8604cf52
Sainath Patil <pspatilsai64@gmail.com>
2023-08-05 16:27:06 +0530

    Set up the react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import "./tailwind.css";
 
 function App() {
-  return <div className="text-3xl text-red-500">App</div>;
+  return (
+    <div className="text-3xl text-red-500">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
